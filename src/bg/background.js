bc3d3a710ace05d21810f4c0060fe7a7ab6fad2e
김기록 <kkr88@nemusoft.co.kr>
2018-02-07 18:13:41 +0900

    - Background에서 combatState 검증하여 Raid Info Popup 생성 / 삭제 하도록 변경(기존 크롬상의 브라우저액션은 Focus용으로 변경 / 해당 효과로 Popup을 강제로 닫아도 다시 생성함)

--- src/bg/background.js
+++ src/bg/background.js
@@ -1,34 +1,117 @@
 "use strict";
 
+var viramateId = 'fgpokpknehglcioijejfeebigdnbnokj'
+var apiUrl = 'chrome-extension://' + viramateId + '/content/api.html';
+
+var isApiLoaded = false;
+var apiHost = null;
+var pendingRequests = {};
+var nextRequestId = 1;
+var enemyStateList = [];
+var apiCallCount = 1000;
+
 var raidInit = false;
 var raidInfoWindow = null;
 
-chrome.browserAction.onClicked.addListener(function(tab) {
-    if (!raidInit) {
-        chrome.windows.getCurrent(function (win) {
-            var width = 440;
-            var height = 600;
-            var left = win.left + win.width;
+function onLoad() {
+    window.addEventListener("message", onMessage, false);
+    tryLoadApi();
+    // API 주기
+    // Viramate example 기준 1초(단위 ms)
+    window.setInterval(tryRefreshCombatState, apiCallCount);
+};
 
-            chrome.windows.create({
-                url: chrome.runtime.getURL("src/window/raid_info.html"),
-                width: width,
-                height: height,
-                //top: Math.round(top),
-                left: Math.round(left),
-                type: 'panel'
-            }, function (window) {
-                raidInfoWindow = window;
-            });
-        });
-        raidInit = true;
-        chrome.windows.onRemoved.addListener(function(windowId) {
-            if(raidInfoWindow.id == windowId) {
-                raidInit = false;
+function tryLoadApi() {
+    console.log("Loading Viramate API");
+    apiHost = document.querySelector("iframe#api_host");
+
+    apiHost.addEventListener("load", onApiLoaded, false);
+    apiHost.src = apiUrl;
+};
+
+function onApiLoaded() {
+    console.log("Viramate API loaded");
+    isApiLoaded = true;
+};
+
+function onMessage(evt) {
+    if (evt.data.type !== "result")
+        return;
+
+    if (evt.data.result && evt.data.result.error) {
+        console.log("Request failed", evt.data.result.error);
+        return;
+    }
+
+    var callback = pendingRequests[evt.data.id];
+    if (!callback)
+        return;
+
+    callback(evt.data.result);
+};
+
+function sendApiRequest(request, callback) {
+    if (!isApiLoaded) {
+        console.log("Viramate API not loaded");
+        callback({ error: "api not loaded" });
+        return;
+    }
+
+    var id = nextRequestId++;
+    request.id = id;
+    pendingRequests[id] = callback;
+
+    apiHost.contentWindow.postMessage(
+        request, "*"
+    );
+};
+
+function tryRefreshCombatState() {
+    sendApiRequest({ type: "getCombatState" }, function (combatState) {
+        if (combatState != null && combatState.enemies != null) {
+            if (!raidInit) {
+                chrome.windows.getCurrent(function (win) {
+                    var width = 440;
+                    var height = 600;
+                    var left = win.left + win.width;
+
+                    chrome.windows.create({
+                        url: chrome.runtime.getURL("src/window/raid_info.html"),
+                        width: width,
+                        height: height,
+                        //top: Math.round(top),
+                        left: Math.round(left),
+                        type: 'panel'
+                    }, function (window) {
+                        raidInfoWindow = window;
+                    });
+                });
+                raidInit = true;
+                chrome.windows.onRemoved.addListener(function (windowId) {
+                    if (raidInfoWindow.id == windowId) {
+                        raidInit = false;
+                        raidInfoWindow = null;
+                    }
+                });
+            } else {
+                // browserAction 버튼 재클릭 시 focus
+                chrome.windows.update(raidInfoWindow.id, { focused: true });
             }
-        });
-    } else {
-        // browserAction 버튼 재클릭 시 focus
-        chrome.windows.update(raidInfoWindow.id, {focused : true});
+        } else {
+            if (raidInfoWindow != null) {
+                chrome.windows.remove(raidInfoWindow.id);
+                raidInit = false;
+                raidInfoWindow = null;
+            }
+        }
+    });
+};
+
+window.addEventListener("DOMContentLoaded", onLoad, false);
+
+// focus 기능만 동작하도록
+chrome.browserAction.onClicked.addListener(function (tab) {
+    if (raidInfoWindow != null) {
+        chrome.windows.update(raidInfoWindow.id, { focused: true });
     }
 });
